89d22f8231660b4878d597b4196faf90b3962800
rainrambler <wanganyu@outlook.com>
2024-11-27 21:41:44 +0800

    For DEBUG

--- chibihash64.go
+++ chibihash64.go
@@ -31,7 +31,7 @@ func chibihash64(keyIn []uint8, len0 int, seed uint64) uint64 {
 
 	h[0] += ((uint64)(len0 << 32)) | ((uint64)(len0 >> 32))
 	if (l & 1) != 0 {
-		h[0] ^= uint64(k[0])
+		h[0] ^= uint64(k[kpos])
 		l--
 		kpos++
 	}
@@ -51,15 +51,23 @@ func chibihash64(keyIn []uint8, len0 int, seed uint64) uint64 {
 		i++
 	}
 
-	for i := 0; l > 0; l -= 2 {
+	i := 0
+	for l > 0 {
+		fmt.Printf("DBG2.1: h(%d) = %x\n", i, h[i])
+		tmp := uint64(k[kpos]) | (uint64(k[kpos+1]) << 8)
+		fmt.Printf("DBG2.1: tmp = %x\n", tmp)
+		h[i] ^= tmp
+		fmt.Printf("DBG2.2: h(%d) = %x, k[0]=%x, k[1]=%x\n",
+			i, h[i], k[kpos], k[kpos+1])
+		h[i] *= P3
+		fmt.Printf("DBG2.3: h(%d) = %x\n", i, h[i])
+		h[i] ^= h[i] >> 31
+
+		fmt.Printf("DBG3: h(%d) = %x\n", i, h[i])
+
+		l -= 2
 		kpos += 2
 		i++
-		if i >= len(h) {
-			break
-		}
-		h[i] ^= (uint64(k[0]) | ((uint64)(k[1] << 8)))
-		h[i] *= P3
-		h[i] ^= h[i] >> 31
 	}
 
 	x := seed
---
+++ chibihash64_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestChibihash641(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		len0  int
+		seed  uint64
+		want  uint64
+	}{
+		// the input-output table
+		// https://github.com/N-R-K/ChibiHash/issues/1
+		{"blank1", "", 0, 0, 0x9EA80F3B18E26CFB},
+		{"blank2", "", 0, 55555, 0x2EED9399FC4AC7E5},
+		{"hi", "hi", 2, 0, 0xAF98F3924F5C80D6},
+		{"123", "123", 3, 0, 0x893A5CCA05B0A883},
+		{"abcdefgh", "abcdefgh", 8, 0, 0x8F922660063E3E75},
+		{"Helloworld", "Hello, world!", 13, 0, 0x5AF920D8C0EBFE9F},
+		{"qwerty1", "qwertyuiopasdfghjklzxcvbnm123456", 32, 0, 0x2EF296DB634F6551},
+		{"qwerty2", "qwertyuiopasdfghjklzxcvbnm123456789", 35, 0, 0x0F56CF3735FFA943},
+	}
+	// The execution loop
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u8arr := []uint8(tt.input)
+			fmt.Printf("Input: %s [%d]: seed: %x\n", tt.input, tt.len0, tt.seed)
+			ans := chibihash64(u8arr, tt.len0, tt.seed)
+			if ans != tt.want {
+				t.Errorf("got %x, want %x", ans, tt.want)
+			}
+		})
+	}
+}
